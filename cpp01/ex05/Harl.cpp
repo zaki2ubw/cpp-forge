a91a101f2f5eb9d8dedf5aefecd1c6d887bfc307
SOHYAMAZ <snow.sohei@gmail.com>
2026-04-11 16:28:01 +0900

    bugfix(factor): add define

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -6,11 +6,16 @@
 /*   By: sohyamaz <sohyamaz@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/11 14:03:30 by sohyamaz          #+#    #+#             */
-/*   Updated: 2026/04/11 14:43:18 by sohyamaz         ###   ########.fr       */
+/*   Updated: 2026/04/11 16:26:24 by sohyamaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
+#include <iostream>
+#include <iomanip>
+
+# define	LEVELRANGE 4
+# define	PREFIX_WIDTH 10
 
 Harl::Harl()
 {
@@ -22,25 +27,35 @@ Harl::~Harl()
 
 void	Harl::complain(std::string state)
 {
-	void(Harl::*funcp[])(void);
-	size_t	levels[];
+	void		(Harl::*funcp[LEVELRANGE])(void);
+	std::string	levels[LEVELRANGE];
+	size_t	i = 0;
 
-	funcp[0] = &Harl::debug();
-	funcp[1] = &Harl::info();
-	funcp[2] = &Harl::warning();
-	funcp[3] = &Harl::error();
+	funcp[0] = &Harl::debug;
+	funcp[1] = &Harl::info;
+	funcp[2] = &Harl::warning;
+	funcp[3] = &Harl::error;
 	levels[0] = "DEBUG";
 	levels[1] = "INFO";
 	levels[2] = "WARNING";
 	levels[3] = "ERROR";
-	while (state != level[i])
+	while (i < LEVELRANGE && levels[i] != state)
 		i++;
-	Harl.funcp[i]();
+	if (i == LEVELRANGE)
+	{
+		std::cerr << std::setw(PREFIX_WIDTH) << "[INVALID]:";
+		std::cerr << "please input error state. ";
+		std::cerr << "You can use only DEBUG/INFO/WARNING/ERROR";
+		std::cerr << std::endl;
+		return ;
+	}
+	(this->*funcp[i])();
 	return ;
 }
 
 void	Harl::debug(void)
 {
+	std::cout << std::setw(PREFIX_WIDTH) << "[DEBUG]:";
 	std::cout << "I love having extra bacon ";
 	std::cout << "for my 7XL-double-cheese-triple-pickle-specialketchup burger. ";
 	std::cout << "I really do!" << std::endl;
@@ -49,6 +64,7 @@ void	Harl::debug(void)
 
 void	Harl::info(void)
 {
+	std::cout << std::setw(PREFIX_WIDTH) << "[INFO]:";
 	std::cout << "I cannot believe adding extra bacon costs more money. ";
 	std::cout << "You didn’t put enough bacon in my burger! ";
 	std::cout << "If you did, I wouldn’t be asking for more!" << std::endl;
@@ -57,6 +73,7 @@ void	Harl::info(void)
 
 void	Harl::warning(void)
 {
+	std::cout << std::setw(PREFIX_WIDTH) << "[WARNING]:";
 	std::cout << "I think I deserve to have some extra bacon for free. ";
 	std::cout << "I’ve been coming for years, ";
 	std::cout << "whereas you started working here just last month." << std::endl;
@@ -65,6 +82,7 @@ void	Harl::warning(void)
 
 void	Harl::error(void)
 {
+	std::cout << std::setw(PREFIX_WIDTH) << "[ERROR]:";
 	std::cout << "This is unacceptable! ";
 	std::cout << "I want to speak to the manager now." << std::endl;
 	return ;
