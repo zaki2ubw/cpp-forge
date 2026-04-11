b380898a581d02637f3623ab88e0e62d92c43ed1
SOHYAMAZ <snow.sohei@gmail.com>
2026-04-11 18:01:20 +0900

    feat(main): make main better

--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sohyamaz <sohyamaz@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/11 15:22:14 by sohyamaz          #+#    #+#             */
-/*   Updated: 2026/04/11 15:44:30 by sohyamaz         ###   ########.fr       */
+/*   Updated: 2026/04/11 17:43:46 by sohyamaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,17 @@ int	main(void)
 
 	std::cout << std::setw(10) << "[START]:";
 	std::cout << "Show sample cases" << std::endl;
+	std::cout << "CASE0: input=''" << std::endl;
 	harl.complain("");
+	std::cout << "CASE1: input=DEBUG" << std::endl;
 	harl.complain("DEBUG");
+	std::cout << "CASE2: input=INFO" << std::endl;
 	harl.complain("INFO");
+	std::cout << "CASE3: input=WARNING" << std::endl;
 	harl.complain("WARNING");
+	std::cout << "CASE4: input=ERROR" << std::endl;
 	harl.complain("ERROR");
+	std::cout << "CASE5: input=HOGE" << std::endl;
 	harl.complain("HOGE");
 	std::cout << std::setw(10) << "[END]:";
 	std::cout << "Show sample cases" << std::endl;
@@ -41,7 +47,7 @@ int	main(void)
 		std::cout << ">";
 		if (!getline(std::cin, line))
 			return 1;
-		if (line == "no")
+		if (line != "yes")
 			break ;
 	}
 	return 0;
