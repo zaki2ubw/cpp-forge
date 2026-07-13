1640ef113f4f3cd95e55d9ab4c7608518cddd3c4
zaki2ubw <snow.sohei@gmail.com>
2026-07-14 04:17:41 +0900

    wip(ex00): too complicated

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -1,81 +1,56 @@
 #include "ScalarConverter.hpp"
 
-void ScalarConverter::convert(const std::string &target) const {}
+void ScalarConverter::convert(const std::string &target) const {
+  if (target.empty())
+    return;
+}
 
-int ScalarConverter::typeDetector(const std::string &target) const {
+namespace {
+
+int typeDetector(const std::string &target) const {
+  if (target.empty())
+    return CASE_INVALID;
+  size_t i = 0;
+  if (!std::isdigit(target[i]) && isPrintableChar(target))
+    return CASE_CHAR;
   if (target == "nan" || target == "nanf")
     return CASE_NAN;
   if (target == "-inf" || target == "+inf" || target == "-inff" ||
       target == "+inff")
     return CASE_INF;
-  size_t length = target.length();
-  if (length == 1 && std::isalpha(target[0]) != 0)
-    return CASE_CHAR;
-  if (!std::isdigit(target[i]) && target[i] != "+" && target[i] != "-")
+  if (target[i] == "+" || target[i] == "-")
+    ++i;
+  size_t len = target.length();
+  size_t num = 0;
+  size_t point = 0;
+  size_t prefixF = 0;
+  for (; i < len; ++i) {
+    if (std::isdigit(target[i]))
+      ++num;
+    else if (target[i] == '.')
+      ++point;
+    else if (i == len - 1 && target[i] == 'f')
+      ++prefixF;
+    else
+      break;
+  }
+  if (num == 0)
     return CASE_INVALID;
-  if (!std::isdigit(target[length - 1]) && target[i] != "f")
+  if (point != 1)
     return CASE_INVALID;
-  for (int i = 1; i < length; ++i) {
-    if (!std)
-      return CASE_INVALID;
-  }
-  return CASE_INT;
-}
+  if (point != 1 && prefixF != 0)
 }
 
-namespace {
-bool isNan(const std::string &target) {
-  if (target == "nan" || target == "nanf")
-    return true;
+bool isPrintableChar(const std::string &target) {
+  if (target.empty())
+    return false;
+  size_t len = target.length();
+  if (len != 1)
+    return false;
+  unsigned char c = static_cast<unsigned char>(target[0]);
+  if (std::isprint(c) == 0)
+    return false;
   else
-    return false;
-}
-
-bool isInf(const std::string &target) {
-  if (target == "-inf" || target == "+inf")
     return true;
-  if (target == "-inff" || target == "+inff")
-    return true;
-  else
-    return false;
 }
-
-bool isChar(const std::string &target) {
-  if (target.length() == 1 && std::isalpha(target[0]) != 0)
-    return true;
-  else
-    return false;
-}
-
-bool isNumber(const std::string &target) {
-  if (!isdigit(target[0]) && target[0] != "+" && target[0] != "-")
-    return false;
-  for (size_t i = 1; i < target.length(); ++i) {
-    if (!std::isdigit(target[i])) {
-      if (i < len - 1) {
-        if (target[i] != ".")
-          return false;
-        else if (std::isdigit(target[i + 1]))
-          return false;
-      }
-      if (i == len - 1) {
-        if (target[i] != "f")
-          return false;
-      }
-    }
-  }
-  return true;
-}
-
-bool isInteger(const std::string &target) {
-  if (!isdigit(target[0]) && target[0] != "+" && target[0] != "-")
-    return false;
-  for (size_t i = 1; i < target.length(); ++i) {
-    if (!isdigit(target[i]))
-      return false;
-  }
-  return true;
-}
-
-bool isSingleFloat(const std::string &target) {
-        for
+} // namespace
--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -8,6 +8,8 @@ public:
   static void convert(const std::string &target) const;
 
 private:
+  enum e_types { INVALIDFORM = 0, CHAR, INT, FLOAT, DOUBLE };
+
   // Forbidden OCF Functions
   ScalarConverter();
   ~ScalarConverter();
